7877a8e8c1a4cb026ffa881b2f63ebb3cf4461a7
Kai Miller <kaihoshijo003@gmail.com>
2024-01-31 16:20:49 -0700

    Found where the site stores their data

--- src/main.py
+++ src/main.py
@@ -1,18 +1,8 @@
 import trade_scrape as ts
-import constants
-
-def write_data_to_file(filename, max_page):
-    with open(filename, "a", newline="") as f:
-        #writer.writerow(constants.COLUMNS)
-        for page_num in range(1, max_page + 1):
-            # Allow for parsing within the website
-            soup = ts.get_page(page_num)
-            # Getting each trade as a row
-            rows = ts.get_trade_rows(soup)
-            for row in rows:
-                print(ts.get_info_on_row(row))
+import json
 
 if __name__ == "__main__":
-    max_page = int(ts.get_max_page())
-    
-    #write_data_to_file("data/trades.csv", 3)
+    val = ts.get_page(1)
+    val = json.loads(val)
+    print(val['data'][0])
+    print(val['data'][0].keys())
--- src/trade_scrape.py
+++ src/trade_scrape.py
@@ -1,14 +1,14 @@
 import requests
 import constants
-from bs4 import BeautifulSoup as bs
+
+def get_all_pages():
+    for num in range(1, 101):
+        yield get_page(num)
 
 def get_page(num=1):
-    print(num)
     # Capitol trades separates page by page for past senator trades
     # ?page= is what signifies what page, the website is on
-    site = constants.TRADE_SITE + f"?page={num}"
-    print(site)
+    site = constants.TRADE_SITE + f"?page={num}&pageSize=100"
     response = requests.get(site)
     content = response.content
-    # Prettifying the data through BeautifulSoup
-    soup = bs(content, "html.parser")
+    return content
