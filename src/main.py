680fc4e5cb4dd7c2b31e3d5371e3aa9b571e7cab
Kai Miller <kaihoshijo003@gmail.com>
2024-02-03 01:15:20 -0700

    Pulled the database down so that requests aren't repeated

--- src/main.py
+++ src/main.py
@@ -4,19 +4,18 @@ import csv
 import json
 
 def write_pages_to_file(filename, max_pages):
+    print(f'Max pages is {max_pages}')
     with open(filename, 'a', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(constants.COLUMNS)
         for page_num in range(1, max_pages + 1):
+            print(f'On page: {page_num}')
             page = ts.get_page(page_num)
             page = json.loads(page)
             for trade in page['data']:
                 writer.writerow(ts.get_desired_info(trade))
 
 if __name__ == '__main__':
-    max_pages = ts.get_page(1, 10)
+    max_pages = ts.get_page(1, 100)
     max_pages = json.loads(max_pages)
-    # write_pages_to_file('data/trades.csv', ts.get_total_pages(max_pages))
-    # write_pages_to_file('data/trades.csv', 2)
-    for trade in max_pages['data']:
-        print(ts.get_desired_info(trade))
+    write_pages_to_file('data/trades.csv', ts.get_total_pages(max_pages))
--- src/trade_scrape.py
+++ src/trade_scrape.py
@@ -1,6 +1,5 @@
 import constants
 import requests
-import json
 
 def get_all_pages(max_pages):
     for num in range(1, max_pages + 1):
@@ -15,8 +14,7 @@ def get_page(num=1,size=100):
     return content
 
 def get_total_pages(content):
-    val = json.loads(content)
-    return val['meta']['paging']['totalPages']
+    return content['meta']['paging']['totalPages']
 
 def get_desired_info(trade):
     name = trade['politician']['firstName'] + ' ' \
@@ -29,7 +27,7 @@ def get_desired_info(trade):
     txType = trade['txType']
     assetType = trade['asset']['assetType']
     assetTicker = trade['asset']['assetTicker']
-    assetTicker = assetTicker if assetTicker is not None else ''
+    assetTicker = assetTicker if assetTicker is not None else 'None'
     tradeSize = trade['size']
     tradeSize = tradeSize if tradeSize is not None else -1
     return [name, party, pubDate, filingDate, txDate, reportingGap, txType,
